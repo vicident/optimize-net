3ac500ccc3fc9350b8046a206c4e986d247daf6f
fsuzanomassa <fvsmassa@gmail.com>
2016-03-15 18:40:28 +0100

    Improve memory track
    Separates the sources of memory use in outputs, buffers and params

--- tests.lua
+++ tests.lua
@@ -11,16 +11,33 @@ local function genericTestForward(model,opts)
   net:evaluate()
   local out_orig = net:forward(input):clone()
 
-  local mem1 = optnet.countUsedMemory(net,input)
+  local mems1 = optnet.countUsedMemory(net, input)
 
   optnet.optimizeMemory(net, input)
 
   local out = net:forward(input):clone()
-  local mem2 = countUsedMemory(net,input)
+  local mems2 = countUsedMemory(net, input)
   tester:eq(out_orig, out, 'Outputs differ after optimization of '..model)
+
+  local mem1 = mems1.total_size
+  local mem2 = mems2.total_size
+
+  local omem1 = mems1.outputs
+  local omem2 = mems2.outputs
+
+  local bmem1 = mems1.buffers
+  local bmem2 = mems2.buffers
+
+  local pmem1 = mems1.params
+  local pmem2 = mems2.params
+
   tester:assertle(mem2, mem1, 'Optimized model uses more memory! '..
   'Before: '.. mem1..' bytes, After: '..mem2..' bytes')
-  print(mem1,mem2, 1-mem2/mem1)
+  print('Memory use')
+  print('Total',  mem1/1024/1024,  mem2/1024/1024, 1-mem2/mem1)
+  print('Outputs',omem1/1024/1024,omem2/1024/1024, 1-omem2/omem1)
+  print('Buffers',bmem1/1024/1024,bmem2/1024/1024, 1-bmem2/bmem1)
+  print('Params', pmem1/1024/1024,pmem2/1024/1024, 1-pmem2/pmem1)
 end
 
 function optest.basic()
